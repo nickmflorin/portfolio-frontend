fedd71f57a771d94b2c51cef0870c55f148ab605
nickmflorin <nickmflorin@gmail.com>
2020-03-10 17:24:05 -0400

    Load Education

--- src/App.js
+++ src/App.js
@@ -37,27 +37,9 @@ function MiniHeader(props) {
 }
 
 class App extends Component {
-  state = {
-    response: ''
-  };
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
   navSelected(){
     console.log('Nav Clicked')
   }
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200){
-      throw Error(body.message);
-    }
-
-    return body;
-  };
   render() {
     return (
         <BrowserRouter>
--- src/pages/resume/resume.js
+++ src/pages/resume/resume.js
@@ -1,13 +1,66 @@
-import React, { Component } from 'react';
+import React from 'react';
 import _ from 'underscore'
 
-class Resume extends Component {
+import getEducation from '../../services/education'
+
+
+class EducationItem extends React.Component {
+  render() {
+    return (
+      <p>
+        {this.props.name}
+      </p>
+    )
+  }
+}
+
+class Education extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
   render() {
     return (
       <div className="page-content">
-      	<h5> Resume </h5>
+      	<h5> Education </h5>
+        {(this.props.items.map((item) => (
+            <EducationItem
+              key={item._id}
+              name={item.name}
+            />
+        )))}
       </div>
     )
   }
 }
+
+
+export class Resume extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {education: []}
+  }
+  componentWillMount() {
+    var self = this
+    getEducation().then((response) => {
+      self.setState({
+          education: response,
+      })
+    }).catch((error) => {
+      console.log('There was an error loading the education.')
+      console.log(error)
+    })
+  }
+  render() {
+    return (
+      <div className="page-content">
+      	<h5> Resume </h5>
+        {this.state.education &&
+          (<Education items={this.state.education}/>)
+        }
+      </div>
+    )
+  }
+}
+
 export default Resume;
