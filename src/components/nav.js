641d261ade4b388cbf4dddb79f2a12827defdfb3
nickmflorin <nickmflorin@gmail.com>
2020-03-17 18:09:10 -0400

    Update NavBar

--- src/components/nav.js
+++ src/components/nav.js
@@ -20,17 +20,24 @@ class NavBarItem extends React.Component {
 
 export class NavBar extends React.Component {
   render() {
+    // TODO: We might want to deprecate this.
     const cls_names = classNames('nav-bar', {
         ['overlay']: this.props.overlay === true,
     });
+    // TODO: Figure out how to set the logo in CSS so we can set it for different
+    // resolutions.
     return (
       <div className={cls_names}>
-        <div className="nav-left">
-          <div className="nav-bar-logo-container">
-            <Link to="/" className="nav-bar-logo">Nick Florin </Link>
+        <div className="container-left">
+          <Link to="/" className="nav-bar-logo">
+          <div className="logo-container">
+            <div className="logo-wrapper">
+              <img className="logo" src="logo192.png" />
+            </div>
           </div>
+          </Link>
         </div>
-        <div className="nav-right">
+        <div className="container-right">
           {this.props.items.map((item) => {
             return <NavBarItem key={item.id} item={item} />
           })}
