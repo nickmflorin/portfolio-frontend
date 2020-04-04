5db9828f8e691cba6334a299b7db1c2e6416ddd0
nickmflorin <nickmflorin@gmail.com>
2020-04-04 15:15:13 -0400

    Move Footer to SASS

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,34 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { pixelfy } from 'utils'
 import { FOOTER } from 'config'
 
-
-const StyledFooter = styled.footer`
-  width: 100%;
-  position: fixed;
-  bottom: 0;
-  background-color: ${props => props.theme.colors.footer};
-  border-top: ${props => props.theme.borders.dark};
-  height: ${props => pixelfy(props.theme.heights.footer)}
-`;
-
-const StyledFooterContent = styled.p`
-  line-height: ${props => pixelfy(props.theme.heights.footer)};
-  color: ${props => props.theme.colors.white};
-  font-size: 12px;
-  text-align: center;
-`;
 
 class Footer extends React.Component {
   render() {
     return (
-      <StyledFooter>
-        <StyledFooterContent>
-          {FOOTER}
-        </StyledFooterContent>
-      </StyledFooter>
+      <footer>
+        <div className='content'>{FOOTER}</div>
+      </footer>
     )
   }
 }
