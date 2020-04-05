9e6fa440c64e5e8dae7ec312ea17a5b1792065dd
nickmflorin <nickmflorin@gmail.com>
2020-04-04 20:05:20 -0400

    Make Item Functional

--- src/components/items/base.js
+++ src/components/items/base.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-
 import { ComponentSpinner } from 'components/spinner'
 
 var classNames = require('classnames');
 
 
-class Item extends React.Component {
-  render(){
-    return (
-      <div {...this.props} className={classNames('item', this.props.className)}>
-        <ComponentSpinner show={this.props.loading || false} />
-        {this.props.children}
-      </div>
-    )
-  }
+const Item = (props) => {
+  return (
+    <div className={classNames('item', props.className)}>
+      <ComponentSpinner show={props.loading || false} />
+      {props.children}
+    </div>
+  )
 }
 
 export default Item;
