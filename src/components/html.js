df4f2dfa819093f1e12c3b31382565f50979ffe5
nickmflorin <nickmflorin@gmail.com>
2020-04-08 19:18:42 -0400

    Build in Security for XSS, Add Linter Config

--- src/components/html.js
+++ src/components/html.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import dompurify from 'dompurify';
+
 import { IconizedHeader } from 'components/icons'
 
 
-export const HtmlDescription = (props) => (
-  <p className='description' dangerouslySetInnerHTML={{__html: props.children}}></p>
-)
+export const HtmlDescription = (props) => {
+  const sanitizer = dompurify.sanitize;
+  return <p className='description' dangerouslySetInnerHTML={{__html: sanitizer(props.children)}}></p>
+}
